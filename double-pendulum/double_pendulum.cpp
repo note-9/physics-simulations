56b359bac2e8898a16a2409ccfc56c65adb6df26
note-9 <heemanshbhawsar2004@gmail.com>
2025-10-17 22:11:51 +0530

    using sfml instead of sdl

--- double-pendulum/double_pendulum.cpp
+++ double-pendulum/double_pendulum.cpp
@@ -1,22 +1,46 @@
-#include <SDL2/SDL.h>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
-#include <cmath>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char *argv[])
+int main()
 {
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 
-    // returns zero on success else non-zero
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
+    // Load a sprite to display
+    const sf::Texture texture("cute_image.jpg");
+    sf::Sprite sprite(texture);
+
+    // Create a graphical text to display
+    const sf::Font font("arial.ttf");
+    sf::Text text(font, "Hello SFML", 50);
+
+    // Load a music to play
+    sf::Music music("nice_music.ogg");
+
+    // Play the music
+    music.play();
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
+        window.display();
     }
-    SDL_Window* win = SDL_CreateWindow("GAME",
-                                       SDL_WINDOWPOS_CENTERED,
-                                       SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
-    while (1)
-        ;
-
-    return 0;
 }
